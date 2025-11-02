6747a7284efd3183d639571faf3897951197e8f4
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-02 12:49:15 +0800

    Add files via upload

---
+++ pertemuan1.py
@@ -0,0 +1,12 @@
+print("NAMA :kevin christian alexander")
+print("NIM  :25241088")
+print("KELAS:PTI C")
+a=1
+x=-7
+print(a+x)
+z=["alex"]
+print(z)
+#cara cpoy 1 baris arahkan kursor lalu tahan Alt+Shift
+#cara cpoy 1 baris arahkan kursor lalu tahan Alt+Shift
+#cara cpoy 1 baris arahkan kursor lalu tahan Alt+Shift
+#memindahkan Alt+atas\bawah
