3d42162acbfbd8c94563e05457776f42a7029f59
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-06 16:36:24 +0800

    Add files via upload
    pertemuan2.py

---
+++ pertemuan2.py
@@ -0,0 +1,34 @@
+#varabel
+#variabel adalah cara kita menyimpan nilai atau value (aljabar)
+#X:variabel =:assigment 13:nilai contoh(X=13)
+#ATURAN PENAMAAN VARIABEL
+#tidak boleh di awali dengan angka contoh 99lex=10
+#penyambungan kata menggunakan _ contoh alex_lucu=10
+#bisa juga menggabungkan dengan huruf kapital contoh alexLucu=10
+
+x=10
+y=20
+lebar=30
+
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lebar =", lebar)
+print("nilai dari x+y =", lebar)
+
+#pemanggilan indirect
+z=x
+print("nilai dari z =", z)
+#maka nilai z yaitu sama dengan x
+
+z=x
+o=y
+print("nilai dari z+o =", z+o)
+
+nama=("kevin christian alexander")
+nim=25241088
+print("nama saya adalah =", nama)
+print("nim saya adalah =", nim)
+#pemanggilan indirect
+q=nama
+e=nim
+print("nama saya adalah", q +" " "dan nim saya adalah", e)
