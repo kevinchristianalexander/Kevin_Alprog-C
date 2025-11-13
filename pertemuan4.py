d80a3b4af9b9cebb703b67045acfdb2c0a6fe520
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-13 17:34:38 +0800

    Add files via upload
    pertemuan4.py

---
+++ pertemuan4.py
@@ -0,0 +1,23 @@
+print("NAMA :kevin christian alexander")
+print("NIM  :25241088")
+print("KELAS:PTI C")
+
+#MENGAMBIL INPUT DARI USER (lex)
+nama=input("masukan nama anda : ")
+print("selamat datang", nama)
+print(type(nama))
+
+#JIKA INGIN MENGAMBIL DATA INTEGER, MAKA (1)
+data_int = int(input("masukan data anda :") )
+print("nilai anda", data_int)
+print(type(data_int) )
+
+#JIKA INGIN MENGAMBIL DATA FLOAT, MAKA (3.1)
+data_float = float(input("masukan data anda :") )
+print("nilai anda", data_float)
+print(type(data_float) )
+
+#JIKA INGIN MENGAMBIL DATA FLOAT, BOOL (KEBENARAN)TRUE
+data_bool = bool(input("masukan data anda :") )
+print("nilai anda", data_bool)
+print(type(data_bool) )
