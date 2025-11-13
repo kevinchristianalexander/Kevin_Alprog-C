a0908ffa41d6cf08e10547009d92b9bdd0f40213
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-13 17:34:00 +0800

    Add files via upload
    pertemuan3.py

---
+++ pertemuan3.py
@@ -0,0 +1,21 @@
+print("NAMA :kevin christian alexander")
+print("NIM  :25241088")
+print("KELAS:PTI C")
+
+#TIPE DATA=TIPE DARI SEBUAH NILAI
+
+x=10#TIPE DATA INTEGER (YSNG BULAT)
+print("nilai dari x =", x)
+print("tipe data dari x=", type(x))
+
+y=3.5#TIPE DATA DARI FLOAT (YANG BERUPA PECAHAN)
+print("nilai dari y =", y)
+print("tipe data dari y=", type(y))
+
+nama=("alex")#TIPE DATA STRING (BERUPA KATA)
+print("nilai dari nama =", nama)
+print("tipe data dari nama=", type(nama))
+
+kebenaran=True#TIPE DATA BOLEAN
+print("nilai dari kebenaran =", kebenaran)
+print("tipe data dari kebenaran=", type(kebenaran))
