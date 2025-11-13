3a0905ad9f11c279f037d308a0aeac3b5a5c8362
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-13 23:25:25 +0800

    Add files via upload
    TUGAS MENGAMBIL INPUT DARI USER

---
+++ tugasmengambilinputdariuser.py
@@ -0,0 +1,28 @@
+#("NAMA :kevin christian alexander")
+#("NIM  :25241088")
+#("KELAS:PTI C")
+
+#TUGAS PROGRAM DARI OUTPUT BERIKUT
+#MASUKAN NAMA ANDA :
+#MASUKAN UMUR ANDA :
+#NAMA ANDA ADALAH (NAMA)
+#TIPE DATA DARI NAMA ANDA ADALAH (TIPE DATA DARI NAMA)
+#UMUR ANDA ADALAH (UMUR)
+#TIPE DATA DARI UMUR ANDALAH (TIPE DATA DARI UMUR)
+
+nama=input("masukan nama anda : ")
+data_int = int(input("masukan umur anda :") )
+print("nama anda adalah", nama)
+print(type(nama))
+print("umur anda adalah", data_int)
+print(type(data_int) )
+
+#HASIL KTIKA DI RUN
+#PS D:\tugasmengambilinputdariuser.py> & "C:/Users/ASUS 16X/AppData/Local/Microsoft/WindowsApps/python3.12.exe" d:/tugasmengambilinputdariuser.py/tugasmengambilinputdariuser.py
+#masukan nama anda : kevin christian alexaander
+#masukan umur anda : 18
+#nama anda adalah kevin christian alexaander
+#<class 'str'>
+#umur anda adalah 18
+#<class 'int'>
+#PS D:\tugasmengambilinputdariuser.py> 
