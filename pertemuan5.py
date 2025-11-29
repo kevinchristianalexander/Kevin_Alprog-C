77a34f7d23bf0096c4b8e38d2f86150775f6e840
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2025-11-29 10:34:06 +0800

    Add files via upload
    pertemuan5.py

---
+++ pertemuan5.py
@@ -0,0 +1,56 @@
+#("NAMA :kevin christian alexander")
+#("NIM  :25241088")
+#("KELAS:PTI C")
+
+# operasi aritmatika
+
+# + - x /
+# penjumlahan (+)
+# pengurangan (-)
+# perkalian (*)
+# pembagian (/)
+# perpangkatan (**)
+# modulus (%)
+# floor division (//)
+
+# penjumlahan 
+a = 9
+b = 2
+nilai = a + b
+print(a, "+", b, "=", nilai)
+
+# pengurangan 
+a = 19
+b = 6
+nilai = a - b
+print(a, "-", b, "=", nilai)
+
+# perkalian
+a = 5
+b = 2
+nilai = a * b
+print(a, "x", b, "=", nilai)
+
+# pembagian
+a = 10
+b = 5
+nilai = a / b
+print(a, ":", b, "=", nilai)
+
+# perpangkatan 
+a = 9
+b = 1
+nilai = a ** b
+print(a, "^", b, "=", nilai)
+
+# modulus
+a = 35
+b = 10
+nilai = a % b
+print(a, "%", b, "=", nilai)
+
+# floor division // pembulatan
+a = 50
+b = 5
+nilai = a // b
+print(a, "//", b, "=", nilai)
